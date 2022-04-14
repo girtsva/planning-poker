aff5b7820dfd76abeed0a79bd39bdfe78802b47b
Girts Varna <gvarna@gmail.com>
2022-04-14 14:50:44 +0300

    refactoring within code review

---
+++ PlanningPoker.Data/DataRepository.cs
@@ -0,0 +1,14 @@
+﻿using PlanningPoker.Models;
+
+namespace PlanningPoker.Data;
+
+public class DataStorage
+{
+    //public static ICollection<Player> Players = new List<Player>();
+    public static readonly ICollection<GameRoom> GameRooms = new List<GameRoom>();
+
+    public bool Exists(string name)
+    {
+        return GameRooms.Any(room => room.Name.ToLower().Trim() == name.ToLower().Trim());
+    }
+}
