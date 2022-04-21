daadbfb48ec01f6c25fd9905711ed7d9a9063b03
Girts Varna <gvarna@gmail.com>
2022-04-21 12:56:05 +0300

    added Serilog

--- PlanningPoker.ClientApi/Program.cs
+++ PlanningPoker.ClientApi/Program.cs
@@ -4,47 +4,72 @@ using PlanningPoker.Data;
 using PlanningPoker.Data.Interfaces;
 using PlanningPoker.Services;
 using PlanningPoker.Services.Interfaces;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
+Log.Information("Starting up...");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+try
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
     {
-        Version = "v1",
-        Title = "Planning Poker Web API",
-        Description = "An ASP.NET Core Web API .NET 6 application that ..."
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Planning Poker Web API",
+            Description = "An ASP.NET Core Web API .NET 6 application that ..."
+        });
+
+        // Set the comments path for the Swagger JSON and UI.
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        options.IncludeXmlComments(xmlPath);
     });
 
-    // Set the comments path for the Swagger JSON and UI.
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    options.IncludeXmlComments(xmlPath);
-});
+    builder.Services.AddTransient<IGameRoomService, GameRoomService>();
+    builder.Services.AddTransient<IDataRepository, DataRepository>();
+    //builder.Services.AddSingleton<IDataRepository, DataRepository>();
+    //builder.Services.AddScoped<IDataRepository, DataRepository>();
 
-builder.Services.AddTransient<IGameRoomService, GameRoomService>();
-builder.Services.AddTransient<IDataRepository, DataRepository>();
-//builder.Services.AddSingleton<IDataRepository, DataRepository>();
-//builder.Services.AddScoped<IDataRepository, DataRepository>();
+    var app = builder.Build();
 
-var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+    app.UseHttpsRedirection();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception e)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Fatal(e, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
