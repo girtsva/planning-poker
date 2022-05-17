c23fb56006d106ffe68e5b741544fb32d298fba0
Girts Varna <gvarna@gmail.com>
2022-05-17 16:45:18 +0300

    NET-5
    Refactored GameRoomExpirationService

--- PlanningPoker.Services/GameRoomExpirationService.cs
+++ PlanningPoker.Services/GameRoomExpirationService.cs
@@ -10,9 +10,8 @@ public class GameRoomExpirationService: IHostedService, IDisposable
     private readonly ILogger<GameRoomExpirationService> _logger;
     private readonly IServiceProvider _services;
     private Timer _timer = null!;
+    private static readonly TimeSpan DaysToKeepGameRoom = TimeSpan.FromDays(5);
     
-    private const int DaysToKeepGameRoom = 5;
-
     public GameRoomExpirationService(ILogger<GameRoomExpirationService> logger, IServiceProvider services)
     {
         _logger = logger;
@@ -23,33 +22,26 @@ public class GameRoomExpirationService: IHostedService, IDisposable
     {
         _logger.LogInformation("Timed Hosted Service is starting..");
 
-        _timer = new Timer(DeleteGameRooms, null, TimeSpan.Zero, TimeSpan.FromHours(1));
+        _timer = new Timer(DeleteGameRooms, _services, TimeSpan.Zero, TimeSpan.FromHours(1));
         
         return Task.CompletedTask;
     }
     
     private void DeleteGameRooms(object? state)
     {
-        using (var scope = _services.CreateScope())
-        {
-            _logger.LogInformation("Timed Hosted Service [DeleteGameRooms] is working..");
+        using var scope = ((IServiceProvider)state!).CreateScope();
+        
+        _logger.LogInformation("Timed Hosted Service [DeleteGameRooms] is working..");
 
-            var scopedProcessingService = scope.ServiceProvider.GetRequiredService<IDataRepository>();
+        var scopedProcessingService = scope.ServiceProvider.GetRequiredService<IDataRepository>();
             
-            var gameRooms = scopedProcessingService.ListGameRooms();
+        var gameRooms = scopedProcessingService.ListGameRooms();
             
-            foreach (var gameRoom in gameRooms)
-            {
-                var dateTimeWhenToDelete = gameRoom.CreatedOn.AddDays(DaysToKeepGameRoom);
-            
-                if (DateTime.UtcNow >= dateTimeWhenToDelete)
-                {
-                    scopedProcessingService.DeleteRoom(gameRoom.ExternalId);
-                }
-            }
-            
-            _logger.LogInformation("Timed Hosted Service [DeleteGameRooms] finished working");
-        }
+        gameRooms.Where(gameRoom => DateTime.UtcNow - gameRoom.CreatedOn >= DaysToKeepGameRoom)
+                .ToList()
+                .ForEach(gameRoom => scopedProcessingService.DeleteRoom(gameRoom.ExternalId));
+        
+        _logger.LogInformation("Timed Hosted Service [DeleteGameRooms] finished working");
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
